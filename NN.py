f538be097922a50dc4179459954c3283e2f99471
vcostale <108403198+vancostle@users.noreply.github.com>
2022-07-14 20:26:51 +0200

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,5 +1,9 @@
 import pandas as pd                                         # loading and cleaning the dataset
 import numpy as np                                          # manipulating it
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+
 import tensorflow as tf
 from tensorflow import keras
 
@@ -10,18 +14,26 @@ path="C:/Users/vanes/OneDrive - Universitat de Barcelona/8e semestre/ML-EELS/"
 ds = path + '/Mn_Fe_dataset.pkl'
 lb = path + '/Mn_Fe_labels.pkl'
 
-ds = pd.read_pickle(ds)
-lb = pd.read_pickle(lb)
+X = pd.read_pickle(ds)
+y = pd.read_pickle(lb)
+
+#X = X.to_numpy()
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.35, random_state = 0)
 
 ####################################### Defining the model
 
 model = keras.Sequential([                                
     keras.layers.Dense(1200, activation='relu', input_shape=(X_train.shape[1],)),          
     keras.layers.Dropout(0.7),
-    keras.layers.Dense(1200/(1200+1+1)),
-    keras.layers.Dense(1200/(1200+2+1)),
-    keras.layers.Dense(1200/(1200+3+1)),
-    keras.layers.Dense(1, activation='softmax')
+    keras.layers.Dense(1200/(2*(1200+1+1))),
+    keras.layers.Dropout(0.7),
+    keras.layers.Dense(1200/(2*(1200+2+1))),
+    keras.layers.Dropout(0.7),
+    keras.layers.Dense(1200/(2*(1200+3+1))),
+    keras.layers.Dropout(0.7),
+    keras.layers.Dense(5, activation='softmax')
 ])
 
 # the last layer need to be 2, affiliation yes (1) or not (0), but with softmax we get their probabilities
@@ -43,5 +55,7 @@ predictions = []
 
 for i in range (0,len(pred_prob)):
     predictions.append(np.argmax(pred_prob[i]))
-    
-print(f'Predicted: "{np.argmax(pred_prob[0])}", Actual: "{(y_test.values)[0]}"')
+
+######################################## Accuracy
+
+print('Accuracy', accuracy_score(y_test, predictions))
